4e8a01540c20585b1b4b7596109b8f34b285f608
Persaud, Gabe <gabriel-persaud@uiowa.edu>
2026-03-29 22:38:22 -0500

    Initial Commit

---
+++ ChatGPT Dark/ChatGPT_DarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_DarkApp.swift
+//  ChatGPT Dark
+//
+//  Created by Gabe Persaud on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_DarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
